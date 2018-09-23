530ddffef05e394627c2e6126e2cef35bde58a08
SinghHarshita <2017.harshita.singh@ves.ac.in>
2018-09-23 22:55:50 +0530

    made few changes

--- registrationpage.php
+++ registrationpage.php
@@ -54,6 +54,40 @@
 </head>
 <body>
 	  	
+<nav class="navbar navbar-default">
+    <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Asset Manager</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Home <span class="sr-only">(current)</span></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active" data-target="#loginModal" data-toggle="modal"><a href="#"><i class="fas fa-user-plus"></i>Sign-Up</a></li>
+        <li><a href="#"><i class="fas fa-user"></i>Login</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
+</nav>
+
+  <div class="container">
+  	<div class="row">
+    	<div class="col-lg-12">
+    		<div id="content">
+    			<h1>Asset Manager</h1>
+    		    <hr>
+    		</div>
+    	</div>
+    </div>
+  </div>
 
 	<div class="container" id="20>
 		<div class="row">
