2dd44f2085ffcb9dab00888e09c9cdcf2a4dcd98
SinghHarshita <2017.harshita.singh@ves.ac.in>
2018-09-23 22:58:27 +0530

    added php code

--- loginpage.php
+++ loginpage.php
@@ -1,3 +1,64 @@
+<?php
+
+	session_start();
+	error_reporting(0);
+	include('dbconn.php');
+	if($_POST['submit'])
+	{
+		$us = $_POST['username'];
+		$pass = $_POST['password'];
+		$qur = "SELECT * from employee where Email_id = '$us' and Password = '$pass'";
+		$result = mysqli_query($ser,$qur);
+		if($result)
+		{
+			echo "Query successful.....<br><br>";
+		}
+		$res = mysqli_num_rows($result);
+		if($res)
+		{
+			$_SESSION['username'] = $us;
+			$res = mysqli_fetch_assoc($result);
+			//echo $us;
+			$_SESSION['id'] = $res['id'];
+			$_SESSION['designation'] = $res['designation'];
+			$d = $_SESSION['designation'];
+			
+			if($d == "staff")
+			{
+				header('location:assetStaff.php');
+			}
+			else if($d == "po")
+			{
+				header("location:po.php");
+			}
+			else if($d == "dpo")
+			{
+				header('location:purchase_incharge.php');
+			}
+			else if($d == "admin")
+			{
+				header('location:amdinview.php');
+			}
+			else if($d == "hod")
+			{
+				header('location:hod.php');
+			}
+			else if($d == "principal")
+			{
+				header('location:principal.php');
+			}
+			else 
+				header('location:other.php');
+		}
+		else
+		{
+			die("Login Failed....");
+		}
+	}
+	
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
