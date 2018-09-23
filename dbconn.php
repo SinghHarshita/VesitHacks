cf9e1a99e8d3145f17105e1d5613f2ebf60d1a86
SinghHarshita <2017.harshita.singh@ves.ac.in>
2018-09-23 15:38:51 +0530

    Connecting to db

---
+++ dbconn.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$servername="localhost";
+	$username="root";
+	$password="hasrhita@99";
+	$dbname="asset_tracker";
+	
+	$connection = mysqli_connect($servername,$username,$password,$dbname);
+	
+?>
